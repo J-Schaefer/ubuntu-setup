cc85e50d55d8746eb0a60f41b5d03576bd4c22cd
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2019-10-17 15:55:57 +0200

    Autoformatting and adding words to folder dictionary.

--- Zotero/better_biblatex_postscript.js
+++ Zotero/better_biblatex_postscript.js
@@ -1,17 +1,22 @@
-if (Translator.BetterBibLaTeX) {    // check for translator
-    switch (item.itemType) {    // check which item currently being translated
-        case 'videoRecording':  // item Video
-        case 'film':    // item Movie
+if (Translator.BetterBibLaTeX) {
+    // check for translator
+    switch (
+    item.itemType // check which item currently being translated
+    ) {
+        case "videoRecording": // item Video
+        case "film": // item Movie
             item.creators.forEach(creator => {
-                if (creator.creatorType === 'director') creator.creatorType = 'author'    // replace director for author
-            })
-            this.addCreators();    // print creators
+                if (creator.creatorType === "director")
+                    creator.creatorType = "author"; // replace director for author
+            });
+            this.addCreators(); // print creators
             break;
-        case 'patent':
+        case "patent":
             item.creators.forEach(creator => {
-                if (creator.creatorType === 'inventor') creator.creatorType = 'author'    // replace holder for author
-            })
-            this.addCreators();    // print creators
+                if (creator.creatorType === "inventor")
+                    creator.creatorType = "author"; // replace holder for author
+            });
+            this.addCreators(); // print creators
             break;
     }
 }
